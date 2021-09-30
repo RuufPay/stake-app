835d63724fa7267e5c1d4349af433dce6f77f353
jgomes79 <jgomes79@hotmail.es>
2021-09-30 10:52:50 +0200

    Updates

---
+++ precache-manifest.e1c4254938d0f9922b2b6103a705fbe1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8ada1fef3c19c47cb3fc01ed0944646",
+    "url": "/index.html"
+  },
+  {
+    "revision": "125e138e98f9f93c8dfc",
+    "url": "/static/js/2.4ea83ab6.chunk.js"
+  },
+  {
+    "revision": "daa74c32336e8ff1de985355885cbaad",
+    "url": "/static/js/2.4ea83ab6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3703d43a8fe6e1e0ed5",
+    "url": "/static/js/main.33a0b696.chunk.js"
+  },
+  {
+    "revision": "4efaf16cadea71a1111a",
+    "url": "/static/js/runtime-main.a08b0100.js"
+  }
+]);
